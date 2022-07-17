7f43c67037017c23a75a2ca108489198f58ccc14
sPreet247 <samre2472@gmail.com>
2022-07-16 21:39:34 -0400

    checkout

--- components/ Navbar.jsx
+++ components/ Navbar.jsx
@@ -1,20 +1,29 @@
-import Link from "next/link.js";
+import Link from "next/link";
 import React from "react";
 import { AiOutlineShopping } from "react-icons/ai";
+
+import { useStateContext } from "../context/StateContext";
 import { Cart } from "./";
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href="/">Beats PRO</Link>
       </p>
 
-      <button type="button" className="cart-icon" onClick="">
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
         <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
-      <Cart />
+
+      {showCart && <Cart />}
     </div>
   );
 };
--- components/ Product.jsx
+++ components/ Product.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+
 import { urlFor } from "../lib/client";
 
 const Product = ({ product: { image, name, slug, price } }) => {
